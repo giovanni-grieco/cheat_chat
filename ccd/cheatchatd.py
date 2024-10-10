a446be5d6d7befc914cd5cba1f21ef84a026abeb
giovanni <giovi.5057@gmail.com>
2024-10-11 00:47:36 +0200

    Bindato listener su 0.0.0.0

--- ccd/cheatchatd.py
+++ ccd/cheatchatd.py
@@ -26,32 +26,47 @@ class CheatChatDaemon:
         settings["local_ip"] = local_ip
         settings["subnet_mask"] = subnet_mask
         self.address_book = address_book.ConcurrentAddressBookProxy()
-        
         self.listen_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.send_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.listen_sock.bind((settings["local_ip"], int(settings["port"])))
+        self.listen_sock.bind(("0.0.0.0", int(settings["port"])))
+        self.listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.listen_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         self.send_sock.bind((settings["local_ip"],47853))
         self.send_sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        self.stop_event = threading.Event()
 
     def run(self):
-        #self.listen_udp()
         listenerThread = threading.Thread(target=self.listen_udp)
         advertiserThread = threading.Thread(target=self.advertise)
         listenerThread.start()
         advertiserThread.start()
+        reader = input("Press enter to stop\n")
+        self.stop_event.set()
         listenerThread.join()
         advertiserThread.join()
 
     def listen_udp(self):
         print(f"Listening on {settings["local_ip"]}:{int(settings["port"])}")
-        while True:
-            data, addr = self.listen_sock.recvfrom(1024)
-            print(f"Received message: {data} from {addr}")
-    
+        self.listen_sock.settimeout(1)
+        while not self.stop_event.is_set():
+            try:
+                data, addr = self.listen_sock.recvfrom(1024)
+                address_string = addr[0]
+                print(f"Received message: {data} from {addr}")
+                if(address_string != settings["local_ip"]):
+                    print(f"Received message: {data} from {addr}")
+            except socket.timeout:
+                continue
+        
+                
+        
+
     def advertise(self):
-        while True:
+        while not self.stop_event.is_set():
             nu.send_udp_packet("CCProto|Hello", settings["broadcast_address"], int(settings["port"]), self.send_sock)
-            time.sleep(10)
+            time.sleep(1)
+        print("Stopping advertiser")
+        nu.send_udp_packet("CCProto|Bye", settings["broadcast_address"], int(settings["port"]), self.send_sock)
 
 if __name__ == "__main__":
     daemon = CheatChatDaemon()
