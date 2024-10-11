5c20eb295568ee75cdd3addf60cdb6f063c971dd
giovanni <giovi.5057@gmail.com>
2024-10-11 09:59:29 +0200

    risolto import

--- ccd/test/discovery_tester.py
+++ ccd/test/discovery_tester.py
@@ -1,5 +1,6 @@
 import socket
-import network_utils as nu
+import time
+from ccd import network_utils
 
 if __name__ == "__main__":
     message = "Hello, UDP!"
@@ -7,4 +8,9 @@ if __name__ == "__main__":
     port = 10011  # Replace with the target port
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
-    nu.send_udp_packet(message, address, port, sock)
+    try:
+        while True:
+            network_utils.send_udp_packet(message, address, port, sock)
+            time.sleep(1)
+    except KeyboardInterrupt:
+        pass
